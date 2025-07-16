b6e4160901c21172ba52b7ac0afcdf78646d9513
Maria Gabriela <seuemail@example.com>
2025-07-17 00:47:30 +0100

    navbar corrigida

--- frontend/src/pages/Equipamentos.jsx
+++ frontend/src/pages/Equipamentos.jsx
@@ -5,7 +5,7 @@ function equipamento() {
     <div className="min-h-screen flex">
       <NavBar />
       <main className="flex flex-col bg-gray-100 p-10">
-        <div className="m-2 rounded-lg ">
+        <div className="m-2 rounded-lg  border-none h-40 ">
 
         </div>
         <h1 className="text-2xl font-bold">Conteúdo Principal</h1>
--- frontend/src/pages/Start.jsx
+++ frontend/src/pages/Start.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import BasicLogin from "../Componentes/Login";
-
-function App() {
-  return (
-    <div className="flex flex-row justify-center items-center bg-bggray">
-      <div className=' w-full'>
-        <BasicLogin/>
-      </div>
-      <div className='flex justify-center w-full'>
-        <img src="logo512.png" />
-      </div>
-    </div>
-  );
-}
-
-=======
 import BasicLogin from "../components/Login";
 
 function App() {
@@ -30,5 +13,4 @@ function App() {
   );
 }
 
->>>>>>> 9059ba2fd88e54fa546bcf157c2d08a137f3cbd9
 export default App;
