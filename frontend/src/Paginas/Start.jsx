433f0155625129192ab38bb32e5fc55270e9c769
opedromonteiro <129053766+opedromonteiro@users.noreply.github.com>
2025-07-16 21:31:46 +0100

    routes added

---
+++ frontend/src/Paginas/Start.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+  return (
+    <div className="flex flex-row justify-center items-center bg-bggray">
+      <div className='bg-green-800 transition-all duration-300 hover:w-hover'>
+        nav
+      </div>
+      <div className='flex justify-center w-full'>
+        <img src="logo512.png" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import StartPage from './Paginas/Start';
+import EquipamentosPage from './Paginas/Equipamentos';
+import PerfilPage from './Paginas/Perfil';
+import SoftwarePage from './Paginas/Software';
+import DashboardPage from './Paginas/Dashboard';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+          <Route index element={<StartPage />} />
+          <Route path="/" element={<StartPage />} />
+          <Route path="equipamentos" element={<EquipamentosPage />} />
+          <Route path="perfil" element={<PerfilPage />} />
+          <Route path="software" element={<SoftwarePage />} />
+          <Route path="dashboard" element={<DashboardPage />} />
+        <Route path="*" element={<div>Página não encontrada</div>} />
+      </Routes>
+    </Router>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +30,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
-
